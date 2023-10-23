0de789b57f6d4f976ae36c4b97d3ac4a3dbc1309
Lailson Santana <lailsonbit@gmail.com>
2023-10-22 22:49:38 -0300

    Commit
    Commit

---
+++ Projeto/img_suavizada/PreProcessamento.py
@@ -0,0 +1,54 @@
+import os
+from PIL import Image
+from PIL import ImageFilter
+import numpy as np
+
+# Diretório que contém as imagens
+diretorio = "Img/"
+
+# Lista para armazenar as imagens
+imagens = []
+
+# Percorra todos os arquivos na pasta
+for nome_arquivo in os.listdir(diretorio):
+    caminho_completo = os.path.join(diretorio, nome_arquivo)
+
+    # Verifique se o arquivo é uma imagem (você pode adicionar mais extensões de arquivo, se necessário)
+    if nome_arquivo.endswith(('.jpg', '.jpeg', '.png', '.gif')):
+        try:
+            imagem = Image.open(caminho_completo)
+            imagens.append(imagem)
+        except Exception as e:
+            print(f"Erro ao abrir a imagem {caminho_completo}: {str(e)}")
+
+# Agora, a lista "imagens" contém todas as imagens da pasta
+cont = 0
+for imagem in imagens:
+    cont = cont + 1
+    #Redimensiona Imagem
+    nova_imagem = imagem.resize((224, 224))
+    #Transforma RGB
+    imagem_rgb = nova_imagem.convert("RGB")
+    #Suaviza para eliminar ruído
+    imagem_suavizada = imagem_rgb.filter(ImageFilter.MedianFilter(size=3))
+    #Faz a normalização
+    #imagem_array = np.array(imagem_suavizada)
+    #imagem_normalizada = (imagem_array - imagem_array.min()) / (imagem_array.max() - imagem_array.min())
+    #Salvar Imagem
+    #imagem_desnormalizada = (imagem_normalizada * 255).astype(np.uint8)
+    #imagem_pillow = Image.fromarray(imagem_desnormalizada)
+    
+    imagem_suavizada.save("imagem" + str(cont) + ".jpg")
+
+
+
+
+
+
+
+
+
+
+
+
+
